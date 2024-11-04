2999c52b2f56c5f4ba6ef70ac2588eac6938f848
yenisleydi <126813935+yenisleydi@users.noreply.github.com>
2024-11-04 09:24:43 -0600

    Login y footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
+import Login from './pages/Login';
+import Footer from './components/Footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    //<App />
+    <>
+        <Login/>
+        <Footer/>
+    </>
 );
