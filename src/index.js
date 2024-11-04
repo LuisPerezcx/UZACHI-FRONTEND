947b43946a1904fe1007f0def4711d5a3eb3e7ea
Ramiro Martinez Santos <martinezsantosramiro79@gmail.com>
2024-11-04 16:23:47 -0600

    vista_reportes_semarnat

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// src/index.js o src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ReportsPage from './pages/ReportsPageSemarnat';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    /*<App />*/
+    <ReportsPage></ReportsPage>
 );
