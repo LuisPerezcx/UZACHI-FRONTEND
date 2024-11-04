b8cbfd6cea186da82c96c5b121901a05d05cc3a0
Gabriela Ramirez Cruz <gabytacruz447@gmail.com>
2024-11-04 09:21:39 -0600

    usuario

---
+++ src/components/EliminarUsuario.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EliminarUsuario = () => {
+  return (
+    <div>EliminarUsuario</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+import { Usuario } from './components/Usuario';
+import { ModificarUsuario } from './components/ModificarUsuario';
+import { EliminarUsuario } from './components/EliminarUsuario';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Usuario />,
+      children: [
+        { path: "/usuario", element: <Usuario /> },
+        { path: "/usuario/modificar/:id", element: <ModificarUsuario /> },
+        { path: "/usuario/eliminar/:id", element: <EliminarUsuario /> }
+      ]
+    }
+  ])
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
