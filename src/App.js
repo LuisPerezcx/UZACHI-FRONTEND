31fa60157dd04516f1e7db935dafb2450b434004
= <24mdavid25@gmail.com>
2024-11-07 17:07:51 -0600

    Panel de folios autorizados

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+  // src/App.js
+  import React from 'react';
+  import CommunityForm from './components/CommunityForm';
+  import CommunityList from './components/CommunityList';
+  import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>UZACHI</h1>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1>UZACHI</h1>
+          <nav>
+            <a href="#">Catálogos</a>
+            <a href="#">Historial</a>
+            <a href="#">Contacto</a>
+          </nav>
+        </header>
+        <div className="content">
+          <h2>Asignar Folios Autorizados</h2>
+          <CommunityForm/>
+          <CommunityList/>
+        </div>
+        <footer className="footer">
+          
+        </footer>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
