b31333d274b45d600e1beb1ac4d2481662f73564
yenisleydi <126813935+yenisleydi@users.noreply.github.com>
2024-11-19 14:41:32 -0600

    login, footer y calculadoa

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ import { PrincipalAdmin } from './pages/PrincipalAdmin';
 import { ReportesInternos } from './pages/ReportesInternos';
 import { ReportesPage } from './pages/ReportsPageSemarnat';
 import { GestionTransporte } from './pages/GestionTransporte';
+import { CalculadoraEspecial } from './components/Calculadora/CalculadoraEspecial';
+import { CalculadoraEstandar } from './components/Calculadora/CalculadoraEstandar';
+import { Calculadoras } from './pages/Calculadoras';
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import LoginForm from './components/Login/LoginForm';
 
 
 const AppRoutes = () => {
@@ -13,6 +19,7 @@ const AppRoutes = () => {
       <Route path="/ReportesInternos" element={<ReportesInternos />} />
       <Route path='/ReportesSemarnat' element={<ReportesPage />}/>
       <Route path='/GestionTransporte' element={<GestionTransporte/>} />
+      <Route path='/prueba' element={<Login/>} />
     </Routes>
   );
 };
