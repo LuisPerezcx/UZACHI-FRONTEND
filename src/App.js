f145b93b9e7462d87e9e41fc61c0c77d7679bdb0
Ramiro Martinez Santos <martinezsantosramiro79@gmail.com>
2024-11-18 23:28:18 -0600

    rama.ramis

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 function App() {
   return (
     <div>
