b88fa8c99ff47c56c8f3c29d6f171e6571f32ee7
LuisPerezcx <luisperez90377@gmail.com>
2024-11-19 18:01:57 -0600

    informacion remitente

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { PrincipalAdmin } from './pages/PrincipalAdmin';
-import { ReportesInternos } from './pages/ReportesInternos';
+import { ReportesInternos } from './pages/ReportesInternos/ReportesInternos';
 import { ReportesPage } from './pages/ReportsPageSemarnat';
 import { GestionTransporte } from './components/GestionTransporte';
 import { Formulario3 } from './pages/Formulario3';
@@ -9,6 +9,7 @@ import { Formulario } from './pages/Formulario';
 import { CalculadoraEspecial } from './components/Calculadora/CalculadoraEspecial';
 import { CalculadoraEstandar } from './components/Calculadora/CalculadoraEstandar';
 import { Calculadoras } from './pages/Calculadoras';
+import { InformacionRemitente } from './components/informacionRemitente/InformacionRemitente';
 
 
 const AppRoutes = () => {
@@ -20,6 +21,7 @@ const AppRoutes = () => {
       <Route path='/GestionTransporte' element={<GestionTransporte/>} />
       <Route path='/Formulario' element={<Formulario/>} />
       <Route path='/Formulario3' element={<Formulario3/>} />
+      <Route path='/Prueba' element={<InformacionRemitente/>} />
 
     </Routes>
   );
