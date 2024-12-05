ecc4e29349e3da56932b1895e729f272e5631d33
Ramiro Martinez Santos <martinezsantosramiro79@gmail.com>
2024-12-05 00:11:46 -0600

    Cambios_login_calculadora

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginForm.css';  
 import logo from '../../assets/Iniciar_sesion.png';
+import { Footer } from '../Footer';
 
 export const LoginForm = () => {
   const [email, setEmail] = useState('');
@@ -42,63 +43,60 @@ export const LoginForm = () => {
     }
   };
   return (
-    <div className="container d-flex align-items-center justify-content-center vh-flex">
-      <div
-        className="card shadow-lg p-4 text-center"
-        style={{ maxWidth: '400px', width: '100%', backgroundColor: '#F6F5F5' }}
-      >
+    <div className="container-login d-flex flex-column align-items-center justify-content-between">
+      <div className="card-login">
         <div className="position-relative mb-4">
-          <img 
-            src={logo} 
-            alt="Logo" 
-            className="img-fluid mb-2" 
-            style={{ width: '75%' }} 
+          <img
+            src={logo}
+            alt="Logo"
+            className="img-login"
           />
+          <h5 style={{ fontWeight: 'bold', color: '#4B4B4B' }}>Iniciar Sesión</h5>
         </div>
         <form>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Usuario</label>
-            <input 
-              type="email" 
-              id="email" 
-              className="form-control form-control-sm rounded" 
+            <label htmlFor="email" className="form-label-login">
+              Usuario
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="form-control form-control-login"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">Contraseña</label>
-            <input 
-              type={showPassword ? 'text' : 'password'} 
-              id="password" 
-              className="form-control form-control-sm rounded"  
+            <label htmlFor="password" className="form-label-login">
+              Contraseña
+            </label>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              className="form-control form-control-login"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-
-          <div className="form-check mb-3 ">
-            <input 
-              type="checkbox" 
-              id="showPasswordCheck" 
-              className="form-check-input" 
+          <div className="form-check mb-3 text-start">
+            <input
+              type="checkbox"
+              id="showPasswordCheck"
+              className="form-check-input"
               checked={showPassword}
               onChange={() => setShowPassword(!showPassword)}
             />
-            <label htmlFor="showPasswordCheck" className="form-check-label">
+            <label htmlFor="showPasswordCheck" className="form-check-label-login">
               Mostrar contraseña
             </label>
           </div>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <button 
-                onClick={handleLogin}
-                type="button" 
-                className="btn btn-success w-45 btn-sm mt-3" 
-                style={{ backgroundColor: 'var(--color-verde)', fontWeight: 'bold', border: 'var(--color-verde)' }}>
-                Ingresar
-            </button>
-           </div>
+          <button
+            onClick={handleLogin}
+            type="button"
+            className="btn btn-success-login btn-sm"
+          >
+            Ingresar
+          </button>
         </form>
       </div>
     </div>
