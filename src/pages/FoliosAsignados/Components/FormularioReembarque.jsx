12c094860f76e497a51874be34169fcfbf44f827
EfrenDLD <24mdavid25@gmail.com>
2024-12-06 17:57:48 -0600

    Subiendo cambios cambiar modal por Swal

--- src/pages/FoliosAsignados/Components/FormularioReembarque.jsx
+++ src/pages/FoliosAsignados/Components/FormularioReembarque.jsx
@@ -1,63 +1,53 @@
-import React, { useState, useEffect } from "react"; 
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import ProgressBar from "react-bootstrap/ProgressBar";
-import IconFaltaInfo from "../../../../../UZACHI-FRONTEND/src/assets/icon-falta-info.png"; // Ajusta la ruta según tu estructura de carpetas
-import { Justify } from "react-bootstrap-icons";
-
+import React, {useState } from 'react';
+import { Form,} from 'react-bootstrap';
+import { Button } from '../../../components/Boton';
+import Swal from "sweetalert2"; 
 
 
 export const FormularioReembarque = () => {
-  const [showConfirmModal, setShowConfirmModal] = useState(false);
-  const [showWarningModal, setShowWarningModal] = useState(false);
-
-  useEffect(() => {
-    let timer;
-    if (showWarningModal) {
-      timer = setTimeout(() => {
-        setShowWarningModal(false);
-      }, 3000, 
-    ); 
-    }
-    return () => clearTimeout(timer);
-  }, [showWarningModal]);
 
   const [formData, setFormData] = useState({
-    volumenAutorizado: "",
-    foliosAutorizados: "",
-    folioInicial: "",
-    folioFinal: "",
-    producto: "",
-    piesTabla: "",
-    medidas: "",
-    clasificacion: "",
-    piezas: "",
-    precio: "",
-    volumenExtra: "",
-    total: "",
+    volumenAutorizado: '',
+    foliosAutorizados: '',
+    folioInicial: '',
+    folioFinal: '',
+    producto: '',
+    piesTabla: '',
+    medidas: '',
+    clasificacion: '',
+    piezas: '',
+    precio: '',
+    volumenExtra: '',
+    total: '',
   });
 
-  const handleInputChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleAddButtonClick = () => {
-    const isFormComplete = Object.values(formData).every((field) => field.trim() !== "");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    if (isFormComplete) {
-      setShowConfirmModal(true);
-    } else {
-      setShowWarningModal(true);
+    if (!formData.folioInicial || !formData.folioFinal || !formData.foliosAutorizados) {
+      Swal.fire({
+        title: 'Datos incompletos',
+        text: `Por favor, llena todos los campos requeridos.`,
+        icon: 'warning',
+        confirmButtonText: 'Aceptar',
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: () => {
+          const confirmButton = Swal.getConfirmButton();
+          confirmButton.style.backgroundColor = 'var(--color-verde)'; // Color verde
+        }
+      });
+      return;
     }
   };
-
-  const handleConfirmSubmit = () => {
-    setShowConfirmModal(false);
-    console.log("Formulario enviado:", formData);
-    alert("Formulario enviado exitosamente");
-  };
+  // Llamar a la función `onAdd` con los datos del formulario
   return (
+    <Form onSubmit={handleSubmit}>
     <div className="container mt-4">
       <div className="tarjeta-border row">
         {/* Primera columna: Formulario de trámites */}
@@ -80,36 +70,36 @@ export const FormularioReembarque = () => {
                 </div>
               </div>
               <div className="mb-3">
-                <label htmlFor="volumenAutorizado" className="form-label">Volumen autorizado</label>
-                <input 
+                <Form.Label htmlFor="volumenAutorizado" className="form-label">Volumen autorizado</Form.Label>
+                <Form.Control 
                   type="text"
                   className="form-control"
                   name="volumenAutorizado"
                   value={formData.volumenAutorizado}
-                  onChange={handleInputChange}
+                  onChange={handleChange}
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="foliosAutorizados" className="form-label">Folios autorizados</label>
-                <input 
+                <Form.Label htmlFor="foliosAutorizados" className="form-label">Folios autorizados</Form.Label>
+                <Form.Control 
                   type="text"
                   className="form-control"
                   name="foliosAutorizados"
                   value={formData.foliosAutorizados}
-                  onChange={handleInputChange}
+                  onChange={handleChange}
                 />
               </div>
               <div className="row">
                 {/* Columna 1: Folio inicial */}
                 <div className="col-md-6">
                   <div className="mb-3">
-                    <label htmlFor="folioInicial" className="form-label">Folio inicial</label>
-                    <input 
+                    <Form.Label htmlFor="folioInicial" className="form-label">Folio inicial</Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="folioInicial"
                       value={formData.folioInicial}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -117,13 +107,13 @@ export const FormularioReembarque = () => {
                 {/* Columna 2: Folio final */}
                 <div className="col-md-6">
                   <div className="mb-3">
-                    <label htmlFor="folioFinal" className="form-label">Folio final</label>
-                    <input 
+                    <Form.Label htmlFor="folioFinal" className="form-label">Folio final</Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="folioFinal"
                       value={formData.folioFinal}
-                      onChange={handleInputChange} 
+                      onChange={handleChange} 
                     />
                   </div>
                 </div>
@@ -141,12 +131,12 @@ export const FormularioReembarque = () => {
                 {/* Columna 1 */}
                 <div className="col-md-3">
                   <div className="mb-2">
-                    <label htmlFor="producto" className="form-label">Producto</label>
-                    <select
+                    <Form.Label htmlFor="producto" className="form-label">Producto</Form.Label>
+                    <Form.Select
                       className="form-select"
                       name="producto"
                       value={formData.producto}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                       required
                     >
                     <option disabled value="">
@@ -155,19 +145,19 @@ export const FormularioReembarque = () => {
                       <option value="tabla">Tabla</option>
                       <option value="tablon">Tablón</option>
                       <option value="rollo">Rollo</option>
-                    </select>
+                    </Form.Select>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="" className="form-label"></label>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="piesTabla" className="form-label">Pies tabla</label>
-                    <input 
+                    <Form.Label htmlFor="piesTabla" className="form-label">Pies tabla</Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="piesTabla"
                       value={formData.piesTabla}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -175,12 +165,12 @@ export const FormularioReembarque = () => {
                 {/* Columna 2 */}
                 <div className="col-md-3">
                   <div className="mb-2">
-                    <label htmlFor="medidas" className="form-label">Medidas</label>
-                    <select
+                    <Form.Label htmlFor="medidas" className="form-label">Medidas</Form.Label>
+                    <Form.Select
                       className="form-select"
                       name="medidas"
                       value={formData.medidas}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                       required
                     >
                     <option disabled value="">
@@ -189,19 +179,19 @@ export const FormularioReembarque = () => {
                       <option>16 x 25</option>
                       <option>18 x 50</option>
                       <option>20 x 75</option>
-                    </select>
+                    </Form.Select>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="" className="form-label"></label>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="piezas" className="form-label">Num. Piezas </label>
-                    <input 
+                    <Form.Label htmlFor="piezas" className="form-label">Num. Piezas </Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="piezas"
                       value={formData.piezas}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -209,12 +199,12 @@ export const FormularioReembarque = () => {
                 {/* Columna 3 */}
                 <div className="col-md-3">
                   <div className="mb-2">
-                    <label htmlFor="clasificacion" className="form-label">Clasificación</label>
-                    <select
+                    <Form.Label htmlFor="clasificacion" className="form-label">Clasificación</Form.Label>
+                    <Form.Select
                       className="form-select"
                       name="clasificacion"
                       value={formData.clasificacion}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                       required
                     >
                     <option disabled value="">
@@ -223,19 +213,19 @@ export const FormularioReembarque = () => {
                       <option>Primera</option>
                       <option>Secundaria</option>
                       <option>Tercera</option>
-                    </select>
+                    </Form.Select>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="" className="form-label"></label>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="precio" className="form-label">Precio</label>
-                    <input 
+                    <Form.Label htmlFor="precio" className="form-label">Precio</Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="precio"
                       value={formData.precio}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -243,83 +233,40 @@ export const FormularioReembarque = () => {
                 {/* Columna 4 */}
                 <div className="col-md-3">
                   <div className="mb-2">
-                    <label htmlFor="volumenExtra" className="form-label">Volumen</label>
-                    <input 
+                    <Form.Label htmlFor="volumenExtra" className="form-label">Volumen</Form.Label>
+                    <Form.Control 
                     type="text"
                     className="form-control"
                     name="volumenExtra"
                     value={formData.volumenExtra}
-                    onChange={handleInputChange}
+                    onChange={handleChange}
                     />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="" className="form-label"></label>
+                    <Form.Label htmlFor="" className="form-label"></Form.Label>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="total" className="form-label">Total</label>
-                    <input 
+                    <Form.Label htmlFor="total" className="form-label">Total</Form.Label>
+                    <Form.Control 
                       type="text"
                       className="form-control"
                       name="total"
                       value={formData.total}
-                      onChange={handleInputChange}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
               </div>
             </form>
           </div>
-          <div className="col-md-12 text-center mt-4">
-            <button className="btn btn-primary" style={{background: "#14C3A2", width:"20%"}} onClick={handleAddButtonClick}>
-              Agregar
-            </button>
         </div>
-        </div>
-        {/* Confirm Modal */}
-      <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmar envío</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>¿Está seguro de enviar el formulario?</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowConfirmModal(false)}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleConfirmSubmit}>
-            Confirmar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      {/* Warning Modal */}
-        <Modal show={showWarningModal} onHide={() => setShowWarningModal(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title><p style={{color:"red"}}>Advertencia</p></Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-          <img
-            src={IconFaltaInfo}
-            alt="Advertencia"
-            style={{
-              width: "35%", // Tamaño de la imagen, puedes ajustarlo
-              height: "35%",
-              marginBottom: "20px"
-            }}
-          />
-          <p style={{
-            justifyContent: "center",
-            textAlign: "center"
-          }}>
-            Faltan campos por llenar. Por favor, complete todos los campos antes de enviar.
-          </p>          
-          </Modal.Body>
-          <Modal.Footer>
-            {/*<Button variant="primary" style={{background: "#14C3A2"}} onClick={() => setShowWarningModal(false)}>
-              Cerrar
-            </Button>*/}
-          </Modal.Footer>
-        </Modal>
       </div>
     </div>
+    <div className="text-center">
+            <button variant="success" type="submit" size="sm">
+              {'Agregar'}
+            </button>
+          </div>
+    </Form>
   );
 };
